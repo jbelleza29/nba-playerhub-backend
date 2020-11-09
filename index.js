6496affacf61cf7e377face15a4b69a78bfad575
Pritz Belleza <pritz.belleza@gmail.com>
2020-11-09 17:18:40 +0800

    add team  and player router

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+var cors = require("cors");
+var playerRouter = require('./api/players').router;
+var teamRouter = require('./api/teams').router
 const app = express();
 const port = 3001;
 
-app.use('/api', require('./api/players').router);
+app.use(cors());
+app.use(express.json());
+app.use('/api', playerRouter);
+app.use('/api', teamRouter);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
