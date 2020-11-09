57d6944b41d773224e85a0531bbdd615ea275862
Pritz Belleza <pritz.belleza@gmail.com>
2020-11-09 17:18:53 +0800

    add crud endpoints

--- api/players.js
+++ api/players.js
@@ -4,14 +4,62 @@ const router = express.Router()
 const Player = require('../models/Player')
 
 router.get('/players', (req, res) => {
-    Player.query()
-      .then(players => {
-        console.log('here');
-        res.json(players)
-      })
-      .catch(err => {
-        console.log(err);
-      })
+  Player.query()
+    .then(players => {
+      res.json(players)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+router.get('/players/:id', (req, res) => {
+  let id = req.params.id;
+  Player.query().findById(id)
+    .then(players => {
+      res.json(players)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+router.post('/players', (req, res) => {
+  Player.query().insert(req.body)
+    .then(player => {
+      res.json(player);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+router.put('/players/:id', (req, res) => {
+  const id = req.params.id;
+
+  Player.query()
+    .patchAndFetchById(id, req.body)
+    .then(player => {
+      console.log(player);
+      res.json(player);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+router.delete('/players/:id', (req, res) => {
+  const id = req.params.id;
+
+  Player.query()
+    .deleteById(id)
+    .then(player => {
+      console.log(player);
+      res.json(player);
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 module.exports = {
