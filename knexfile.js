6f02dadc2b8ccf2a6d2052ba9ab1e2db9b6b99d3
Pritz Belleza <pritz.belleza@gmail.com>
2020-11-10 23:29:28 +0800

    add testing config

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,26 @@
-// Update with your config settings.
+
+const path = require("path")
 
 module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/nba_dev',
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: path.join(__dirname, 'db/migrations')
     },
     seeds: {
-      directory: __dirname + '/db/seeds/'
+      directory: path.join(__dirname, 'db/seeds/')
     }
   },
+  test: {
+    client: "sqlite3",
+    connection: ":memory:",
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.join(__dirname, 'db/migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db/seeds')
+    }
+  }
 };
